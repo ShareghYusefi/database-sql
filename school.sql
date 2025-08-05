67a4d0ac84ad627195add6b73ec85540dd9bb7b8
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-08-05 17:16:29 -0600

    DDL

---
+++ school.sql
@@ -0,0 +1,67 @@
+-- This is a comment in a SQL file
+-- What is a Database and Data? 
+-- Data is a fact about an object or event.
+-- Database is a collection of related data records.
+
+-- What are Database Management Systems (DBMS)? 
+-- A DBMS is a software that allows users to create, store, retrieve, update, and manage data in a database.
+-- There are different types of DBMS's, such Herarchical, Network, Relational, Object-Oriented, and NoSQL. 
+
+-- What is a Relational DBMS? 
+-- This is essentially when we store data in tables which have rows and columns.
+
+-- What is SQL? 
+-- SQL stands for Structured Query Language. It is a standar language for accessing and manipulating databases. 
+
+-- What is a Database Schema? 
+-- A database schema is a blueprint of how the database is constructed. It defines the tables, fields, relationships, etc.. 
+
+-- What makes up a SQL Query?
+-- A SQL query is made up of clauses, expressions, predicates, queries, and statements. 
+-- Queries are used to retrieve data from a database. They are used to perform operations such as SELECT, FROM, WHERE, etc. 
+-- Statements are used to perform operations such as CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, and TRUNCATE.
+-- Predicates are used to filter data based on conditions: There are WHERE, HAVING, JOIN.
+
+-- Data Definition Language (DDL)?
+-- DDL is used to define the structure of the database. This includes commands like CREATE, ALTER, DROP, TRUNCATE, and RENAME. 
+-- Common Data types: INT, VARCHAR, CHAR, TEXT, DATE, TIME, TIMESTAMP, and BLOB.
+
+-- Creating a Database
+-- CREATE DATABASE school;
+
+-- The USE statement selects a database. 
+USE school;
+
+-- Create a students table
+CREATE TABLE students (
+    id int,
+    name varchar(100), -- varchar is used to store variable-length character strings.
+    email varchar(100),
+    age int
+);
+
+-- Alter students table to add a new column
+ALTER TABLE students
+ADD `password` varchar(100);
+
+-- Modify the data type of a column
+ ALTER TABLE students
+ MODIFY COLUMN `name` varchar(150);
+ 
+ -- Renaming a column 
+ ALTER TABLE students
+ CHANGE COLUMN name fullname varchar(100);
+ 
+ -- Drop/Delete a column
+ ALTER TABLE students
+ DROP COLUMN age;
+ 
+ -- Drop/Delete Table
+ -- DROP TABLE students;
+
+
+
+
+
+
+
